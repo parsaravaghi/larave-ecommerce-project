b6e09a1779e548a5f9a995308f467fd3dcd683d5
parsaravaghi <parsadox4@gmail.com>
2025-09-13 09:26:26 +0330

    set new prefexes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
+use App\Http\Controllers\Api\Admin\AdminProductCotroller;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ProductCotroller;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('/auth')->group(function(){
+    Route::post("/register" , [AuthController::class , "register"]);
+    Route::post("/login" , [AuthController::class , "login"]);
+});
+
+Route::prefix('/product')->group(function(){
+    Route::get('/{id}' , [ProductCotroller::class , "show"]);
+    Route::get('/' , [ProductCotroller::class , "search"]);
+});
+
+Route::prefix("/admin")->group(function(){
+    Route::prefix("product")->group(function(){
+        Route::post("/" , [AdminProductCotroller::class , "store"]);
+        Route::put("/{id}" , [AdminProductCotroller::class , "edit"]);
+        Route::delete("/{id}" , [AdminProductCotroller::class , "remove"]);
+    });
 });
