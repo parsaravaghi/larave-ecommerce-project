340ffa92c1b1a9e6df29b459cf295a657679252a
parsaravaghi <parsadox4@gmail.com>
2025-09-22 01:29:10 +0330

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,18 @@
 <?php
 
-use App\Http\Controllers\Api\Admin\AdminProductCotroller;
-use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\Api\ProductCotroller;
+use App\Http\Controllers\Api\{
+    AuthController ,
+    CategoryController ,
+    ProductCotroller ,
+};
+
+use App\Http\Controllers\Api\Admin\{
+    AdminCategoryController ,
+    AdminProductCotroller
+};
+
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::prefix('/auth')->group(function(){
     Route::post("/register" , [AuthController::class , "register"]);
@@ -19,10 +24,21 @@ Route::prefix('/product')->group(function(){
     Route::get('/' , [ProductCotroller::class , "search"]);
 });
 
-Route::prefix("/admin")->group(function(){
-    Route::prefix("product")->group(function(){
+Route::prefix("/category")->group(function(){
+    Route::get('/{id}' , [CategoryController::class , "show"]);
+    Route::get('/' , [CategoryController::class , "search"]);
+    Route::get('/parent/{id}' , [CategoryController::class , "showParentIDs"]);
+});
+
+Route::prefix('/admin')->group(function(){
+    Route::prefix("/product")->group(function(){
         Route::post("/" , [AdminProductCotroller::class , "store"]);
-        Route::put("/{id}" , [AdminProductCotroller::class , "edit"]);
-        Route::delete("/{id}" , [AdminProductCotroller::class , "remove"]);
+        Route::put("/{id}" , [AdminProductCotroller::class , "update"]);
+        Route::delete("/{id}" , [AdminProductCotroller::class , "delete"]);
+    });
+    Route::prefix('/category')->group(function() {
+        Route::post('/' , [AdminCategoryController::class , "store"]);
+        Route::put('/{id}' , [AdminCategoryController::class , "update"]);
+        Route::delete('/{id}' , [AdminCategoryController::class , "delete"]);
     });
 });
